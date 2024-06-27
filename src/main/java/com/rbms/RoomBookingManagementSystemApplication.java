c13447af7f31786d1f93dbc7b994a01c6f0ce9fc
Deepali Gupta <deepali.gupta@ibm.com>
2024-06-27 22:18:49 +0530

    Fix: Updating dependencies and replacing javax with jakarta

--- src/main/java/com/rbms/RoomBookingManagementSystemApplication.java
+++ src/main/java/com/rbms/RoomBookingManagementSystemApplication.java
@@ -2,8 +2,10 @@ package com.rbms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.rbms"})
+@EntityScan("com.rbms.model")
 public class RoomBookingManagementSystemApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/rbms/RoomBookingManagementSystemApplicationTests.java
+++ src/test/java/com/rbms/RoomBookingManagementSystemApplicationTests.java
@@ -1,16 +1,15 @@
 package com.rbms;
 
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -19,7 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.rbms.controller.RoomBookingController;
 import com.rbms.service.RoomBookingServiceImpl;
 
-@RunWith(SpringRunner.class)
+@SpringBootTest
 @WebMvcTest(value=RoomBookingController.class)
 public class RoomBookingManagementSystemApplicationTests {
 	
